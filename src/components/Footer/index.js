ca229078adf6ee250eb807a786e75c5ef5688bc8
mcc <costanza+git@gmail.com>
2021-08-07 00:52:44 -0700

    add footer component, links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div className="row">
-      <div className="col-12 text-center">
-        Footer
-      </div>
-    </div>
+    <footer>
+      <p>
+        <a href="https://www.linkedin.com/in/costanza/" rel="noreferrer" target="_blank"><i class="fab fa-linkedin fa-2x"></i></a>
+        <a href="https://github.com/costanza13" rel="noreferrer" target="_blank"><i className="fab fa-github fa-2x"></i></a>
+        <a href="https://www.facebook.com/michael.costanza" rel="noreferrer" target="_blank"><i class="fab fa-facebook-square fa-2x"></i></a>
+      </p>
+    </footer>
   )
 };
 
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -9,9 +9,9 @@ const Navigation = (props) => {
   return (
     <nav>
       <ul>
-        <li className={`${currentSection === 'about' ? 'selected' : ''}`}><a href="#about" onClick={() => setCurrentSection('about')}>About Me</a></li>
+        <li className={`${currentSection === 'about' ? 'selected' : ''}`}><a href="#about" onClick={() => setCurrentSection('about')}>About</a></li>
         <li className={`${currentSection === 'portfolio' ? 'selected' : ''}`}><a href="#portfolio" onClick={() => setCurrentSection('portfolio')}>Portfolio</a></li>
-        <li className={`${currentSection === 'contact' ? 'selected' : ''}`}><a href="#contact" onClick={() => setCurrentSection('contact')}>Contact Me</a></li>
+        <li className={`${currentSection === 'contact' ? 'selected' : ''}`}><a href="#contact" onClick={() => setCurrentSection('contact')}>Contact</a></li>
         <li className={`${currentSection === 'resume' ? 'selected' : ''}`}><a href="#resume" onClick={() => setCurrentSection('resume')}>Resumé</a></li>
       </ul>
     </nav>
